d1a092138a29aa75049a2011adc7f79dc4f75a20
HyhyY190 <fathinkuni@gmail.com>
2023-12-18 15:40:17 +0700

    add

--- member/about.php
+++ member/about.php
@@ -59,9 +59,8 @@
     <div class="box-header">
     <h2><b><p align="center" >INFO LOKASI</p></b></h2><hr>
 
-    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.3252384025172!2d106.93778407921896!3d-6.220773666544059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x807d608b7994382f!2sWarung+Master!5e0!3m2!1sid!2sid!4v1516162744126" width="600" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></p>
-<p>Jl. Mawar Merah Raya No.3 RT 09/07 (Depan SMK Malaka 23/ITBU) Kel. Malaka Jaya - Jakarta Timur</p>
-<p>Telp. 0857-1973-7572 / 0812-9922-1241</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d15810.687787015784!2d110.4142924!3d-7.824503849999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sid!2sid!4v1702863454909!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><p>Jl. Mawar Merah Raya No.3 RT 09/07 (Depan SMK Malaka 23/ITBU) Kel. Malaka Jaya - Jakarta Timur</p>
+<p>Telp. 081226840578</p>
       
     </div>
     </div>
